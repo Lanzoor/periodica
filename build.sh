10cbc4afc4045dceb6a872813058513d7a1e1300
Lanzoor <blessingmom0928@gmail.com>
2025-06-14 20:25:39 +0900

    Add message outputs to keep track

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-python3 -m venv venv
+
+if [ ! -d "venv" ]; then
+  echo "Creating a virtual environment for dependencies..."
+  python3 -m venv venv
+fi
+
+echo "Activating virtual environment and installing dependencies..."
 source venv/bin/activate
 pip install -r requirements.txt
 deactivate
+
+echo "Done! Now feel free to follow the subsequent steps in execution."
