28dbcf3d17478b0dac476b19686d79a8b44199e0
Lanzoor <blessingmom0928@gmail.com>
2025-07-02 20:26:36 +0900

    Update build.py for more cross-platform support

--- build.py
+++ build.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
-import platform
 import os
+import platform
 import subprocess
 import sys
 
-os_name = platform.system()
+OS = platform.system()
+venv_path = "venv"
 
-if os_name == "Windows":
-    if "venv" not in os.listdir():
-        print("Virtual environment was not found; let's create one!")
-        subprocess.run(["python", "-m", "venv", "venv"], check=True)
-    else:
-        print("The virtual environment was found here already... but how?")
-
-    print("Installing packages into the virtual environment...")
-    subprocess.run(["venv\\Scripts\\python.exe", "-m", "pip", "install", "-r", "requirements.txt"], check=True)
-
-    print("You're all set up! Now feel free to continue following instructions in the README.md file.")
-
-elif os_name in ["Linux", "Darwin"]:
-    if "venv" not in os.listdir():
-        print("Virtual environment was not found; let's create one!")
-        subprocess.run(["python3", "-m", "venv", "venv"], check=True)
-    else:
-        print("The virtual environment was found here already... but how?")
-
-    print("Installing packages into the virtual environment...")
-    subprocess.run(["venv/bin/python", "-m", "pip", "install", "-r", "requirements.txt"], check=True)
-
-    print("You're all set up! Now feel free to continue following instructions in the README.md file.")
+if OS == "Windows":
+    py_cmd = "python"
+    venv_python = os.path.join(venv_path, "Scripts", "python.exe")
+elif OS in ["Linux", "Darwin"]:
+    py_cmd = "python3"
+    venv_python = os.path.join(venv_path, "bin", "python")
 else:
-    print(f"Uh oh! You are running this on an unexpected and unsupported OS: {os_name}")
+    print(f"Unsupported OS: {OS}. Aborting...")
     sys.exit(1)
+
+if not os.path.exists(venv_path):
+    print("Virtual environment was not found. Creating one...")
+    subprocess.run([py_cmd, "-m", "venv", venv_path], check=True)
+else:
+    print("Virtual environment already exists. Moving on...")
+
+print("Upgrading pip... (just in case something goes wrong)")
+subprocess.run([venv_python, "-m", "pip", "install", "--upgrade", "pip"], check=True)
+
+print("Installing project dependencies...")
+
+if os.path.exists("requirements.txt"):
+    subprocess.run([venv_python, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
+elif os.path.exists("pyproject.toml"):
+    subprocess.run([venv_python, "-m", "pip", "install", "-e", "."], check=True)
+else:
+    print("Neither requirements.txt nor pyproject.toml found. Please make sure you got the right files.")
+    sys.exit(1)
+
+print("\nSetup complete! Now feel free to continue the installation process.")
